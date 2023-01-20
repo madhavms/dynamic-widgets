29285eaffc88410936e1b633e944e309ecfd636a
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-01-20 13:03:53 +0000

    Application Bootstrap
    Perform bootstrap actions such as getting application config and styles before performing render using a self executing function.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const getConfig = async () => {
+  const config = ["MUIButton", "MUIBadge"];
+  return config;
+};
+
+const getStyles = async () => {
+  const styles = [
+    {
+      MUIButton: {
+        variant: "contained",
+        color: "primary",
+        style: {
+          backgroundColor: "#4CAF50",
+          color: "white",
+          padding: "10px 20px",
+          fontWeight: "bold",
+          borderRadius: "5px",
+        },
+        onClick: () => {
+          console.log("Button clicked");
+        },
+      },
+    },
+    {
+      MUIBadge: {
+        color: "secondary",
+        style: {
+          padding: "10px 20px",
+          fontWeight: "bold",
+          borderRadius: "5px",
+        },
+      },
+    },
+  ];
+
+  return styles
+};
+
+(async () => {
+  const configProps = await getConfig();
+  const styleProps = await getStyles();
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(
+    <React.StrictMode>
+      <App {...{configProps, styleProps}} />
+    </React.StrictMode>
+  );
+})();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
