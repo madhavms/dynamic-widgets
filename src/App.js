197d93d9c4a0fd8139439a836e7692e1d39a4ea8
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-01-20 13:02:35 +0000

    Dynamic Component Loading
    Here dynamic imports are used so that webpack does chunking of the modules and bundles them. These are not part of the initial bundle.
    
    They are loaded via the network when invoked.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useEffect } from "react";
 
-function App() {
+export const componentRegistry = {
+  MUIButton: React.lazy(() => import("@material-ui/core/Button")),
+  MUIBadge: React.lazy(() => import("@material-ui/core/Badge")),
+};
+
+function App({ configProps, styleProps}) {
+  const [Widget, setWidget] = React.useState(null);
+  const [config, setConfig] = React.useState([]);
+  const [styles, setStyles] = React.useState([]);
+  const [currentStyle, setCurrentStyle] = React.useState({});
+
+  useEffect(() => {
+    setConfig([...configProps]);
+    setStyles([...styleProps]);
+  }, [configProps, styleProps]);
+
+  const widgetClickHandler = (widgetName) => {
+    if (!!componentRegistry[widgetName]) {
+      setWidget(componentRegistry[widgetName]);
+      const widgetStyle = styles.filter(style => style.hasOwnProperty(widgetName))[0][widgetName]
+      setCurrentStyle(widgetStyle);
+      console.log(widgetStyle)
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +42,23 @@ function App() {
         >
           Learn React
         </a>
+        <br />
+        <br />
+        {Widget && (
+          <React.Suspense fallback={<div>Loading...</div>}>
+            <Widget {...currentStyle}>Widget</Widget>
+          </React.Suspense>
+        )}
+        <br />
+        <br />
+        {config.map((item) => (
+          <div key={item}>
+          <button onClick={(e) => widgetClickHandler(item)} key={item}>
+           {item}
+          </button>
+          <br/><br/>
+          </div>
+        ))}
       </header>
     </div>
   );
